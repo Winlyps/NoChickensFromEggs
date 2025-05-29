59a620188add29809cf3f1c6e9e4439a9c37ed2e
Winlypse <nts3@o2.pl>
2025-05-29 18:55:40 +0200

    code!

--- src/main/kotlin/winlyps/noChickensFromEggs/NoChickensFromEggs.kt
+++ src/main/kotlin/winlyps/noChickensFromEggs/NoChickensFromEggs.kt
@@ -1,14 +1,29 @@
 package winlyps.noChickensFromEggs
 
+import org.bukkit.entity.EntityType
+import org.bukkit.event.EventHandler
+import org.bukkit.event.Listener
+import org.bukkit.event.entity.CreatureSpawnEvent
 import org.bukkit.plugin.java.JavaPlugin
 
-class NoChickensFromEggs : JavaPlugin() {
+class NoChickensFromEggs : JavaPlugin(), Listener {
 
     override fun onEnable() {
-        // Plugin startup logic
+        // Register the event listener
+        server.pluginManager.registerEvents(this, this)
+        logger.info("NoChickensFromEggs plugin has been enabled!")
     }
 
     override fun onDisable() {
-        // Plugin shutdown logic
+        logger.info("NoChickensFromEggs plugin has been disabled!")
+    }
+
+    @EventHandler
+    fun onCreatureSpawn(event: CreatureSpawnEvent) {
+        // Check if the spawning entity is a chicken and the spawn reason is EGG
+        if (event.entityType == EntityType.CHICKEN && event.spawnReason == CreatureSpawnEvent.SpawnReason.EGG) {
+            // Cancel the spawn event to prevent chicken from spawning
+            event.isCancelled = true
+        }
     }
 }
