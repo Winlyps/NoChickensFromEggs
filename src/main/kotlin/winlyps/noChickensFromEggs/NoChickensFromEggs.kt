569d0ea8a1fe133006b558fe39baf86af74d3872
Winlypse <nts3@o2.pl>
2025-05-29 18:55:27 +0200

    Initial commit

---
+++ src/main/kotlin/winlyps/noChickensFromEggs/NoChickensFromEggs.kt
@@ -0,0 +1,14 @@
+package winlyps.noChickensFromEggs
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class NoChickensFromEggs : JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
